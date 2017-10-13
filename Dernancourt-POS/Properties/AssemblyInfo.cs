cb8b76305d1f51bb999691714b62526711324751
Keano Porcaro <porkd002@mymail.unisa.edu.au>
2017-10-13 13:29:53 +1030

    test

--- Dernancourt-POS/Properties/AssemblyInfo.cs
+++ Dernancourt-POS/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dernancourt-POS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Official Dernancourt Dial-A-Pizza Point of Sale System")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://keano.me")]
 [assembly: AssemblyProduct("Dernancourt-POS")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Keano Porcaro 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
